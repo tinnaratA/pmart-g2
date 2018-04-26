38137d6c4ce47c1c1729222bc2d7026e0f27c062
Thanut Pinsirodom <thanut131036@gmail.com>
2018-04-27 01:39:17 +0700

    Create Product Models

---
+++ horizont-portal/mdm/product/models.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+from common.models.abstracts import TimeStampMixin
+from merchandise.models import MerchandiseMasterItem, UnitOfMeasurement
+# from customer_store.models import CustomerStoreType
+
+# Customer store SKU
+class ProductItem(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+    uom = models.ForeignKey(
+        UnitOfMeasurement,
+        related_name="product_items",
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True
+    )
+    merchandise = models.ForeignKey(MerchandiseMasterItem, related_name="product_items", on_delete=models.CASCADE)
+    store_type = models.ForeignKey(
+        to="customer_store.CustomerStoreType",
+        related_name="product_items",
+        on_delete=models.SET_DEFAULT,
+        default=None,
+        null=True
+    )
+
+    class Meta:
+        app_label = 'product'
+        db_table = 'product_item'
