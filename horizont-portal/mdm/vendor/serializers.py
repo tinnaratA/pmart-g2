f762fb8b38b569ba77b5ece1d51f9bf27f27d085
Thanut Pinsirodom <thanut131036@gmail.com>
2018-05-16 23:54:24 +0700

    vendor serializer

--- horizont-portal/mdm/vendor/serializers.py
+++ horizont-portal/mdm/vendor/serializers.py
@@ -5,6 +5,9 @@ from common.serializers import MdmBaseSerializer
 from common.serializers import HumanNameSerializer
 from common.serializers import AddressSerializer
 
+from merchandise.serializers import MerchandiseMasterItemSerializer
+from merchandise.serializers import UnitOfMeasurementSerializer
+
 
 class VendorContactSerializer(MdmBaseSerializer):
     type = serializers.CharField(max_length=20)
@@ -37,4 +40,41 @@ class VendorSerializer(MdmBaseSerializer):
         )
 
 
+class PurchaseItemImageSerializer(MdmBaseSerializer):
 
+    class Meta:
+        model = models.PurchaseItemImage
+        fields = (
+            'resource_type',
+            'url'
+        )
+
+
+class PurchaseItemSerializer(MdmBaseSerializer):
+    sku = serializers.CharField(max_length=50)
+    uom = UnitOfMeasurementSerializer(many=False)
+    merchandise = MerchandiseMasterItemSerializer(many=False)
+    images = PurchaseItemImageSerializer(many=True)
+
+    class Meta:
+        model = models.PurchaseItem
+        fields = (
+            'resource_type',
+            'sku',
+            'uom',
+            'merchandise',
+            'images'
+        )
+
+
+class VendorPurchaseItemSerializer(MdmBaseSerializer):
+    purchase = PurchaseItemSerializer(many=False)
+    vendor = VendorSerializer(many=False)
+
+    class Meta:
+        model = models.VendorPurchaseItem
+        fields = (
+            'resource_type',
+            'purchase',
+            'vendor'
+        )
